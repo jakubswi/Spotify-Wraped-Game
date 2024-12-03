ac6d90da6ace3f0a66f5026efa5eb0456b366120
jakubswi <jakubswidlowski@gmail.com>
2024-12-03 11:26:06 +0100

    fix style and changing how the playlist id is stored

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import ast
 import os
 import random
-import requests
 
+import requests
 from flask import Flask, render_template, redirect, url_for, request, session
 from flask_bootstrap import Bootstrap5
+# from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 TOKEN_DATA = ''
-SCOPE = "playlist-modify-private playlist-modify-public user-read-private user-read-email"
+PLAYLIST_ID=''
+SCOPE = "playlist-modify-private playlist-modify-public user-read-private user-read-email user-read-currently-playing user-read-playback-state"
 CLIENT_ID = os.environ["CLIENT_ID"]
 CLIENT_SECRET = os.environ["CLIENT_SECRET"]
 CALLBACK_URL = os.environ["CALLBACK_URL"]
@@ -18,19 +20,38 @@ CALLBACK_URL = os.environ["CALLBACK_URL"]
 
 class SongForm(FlaskForm):
     song = StringField('Song', validators=[DataRequired()])
-    submit = SubmitField('Serach')
+    submit = SubmitField('Search')
 
 
 class PlaylistForm(FlaskForm):
     playlist = StringField('Playlist', validators=[DataRequired()])
     submit = SubmitField('Done')
 
-
+# db = SQLAlchemy()
 app = Flask(__name__)
 Bootstrap5(app)
 csrf = CSRFProtect(app)
 app.secret_key = os.environ["SECRET_KEY"]
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+# db.init_app(app)
 index = 0
+
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String, unique=True, nullable=False)
+#     author = db.Column(db.String)
+#     rating = db.Column(db.String)
+#
+# with app.app_context():
+#     db.create_all()
+#     user = User(
+#         title="Harry Potter",
+#         author="J.K. Rowling",
+#         rating="8/10"
+#         )
+#     db.session.add(user)
+#     db.session.commit()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -51,19 +72,19 @@ def adding_to_playlist():
     global index
     list_of_songs_to_add = session.get('added_songs')
     if index == 0:
-        requests.post(f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks',
+        requests.post(f'https://api.spotify.com/v1/playlists/{PLAYLIST_ID}/tracks',
                       headers={'Authorization': 'Bearer ' + TOKEN_DATA, 'Content-Type': 'application/json'},
                       json={'uris': [list_of_songs_to_add[0]['URI']], 'position': 0})
         list_of_songs_to_add.pop(0)
         index += 1
         for song in list_of_songs_to_add:
-            requests.post(f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks',
+            requests.post(f'https://api.spotify.com/v1/playlists/{PLAYLIST_ID}/tracks',
                           headers={'Authorization': 'Bearer ' + TOKEN_DATA, 'Content-Type': 'application/json'},
                           json={'uris': [song['URI']], 'position': random.randint(0, index)})
             index += 1
     else:
         for song in list_of_songs_to_add:
-            requests.post(f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks',
+            requests.post(f'https://api.spotify.com/v1/playlists/{PLAYLIST_ID}/tracks',
                           headers={'Authorization': 'Bearer ' + TOKEN_DATA, 'Content-Type': 'application/json'},
                           json={'uris': [song['URI']], 'position': random.randint(0, index)})
             index += 1
@@ -93,7 +114,7 @@ def removing_from_list():
 
 @app.route('/playlist', methods=['GET', 'POST'])
 def admin():
-    global playlist_id, index
+    global PLAYLIST_ID, index
     session['added_songs'] = []
     if not TOKEN_DATA:
         return redirect(
@@ -104,9 +125,9 @@ def admin():
         if form.validate_on_submit():
             playlist_name = form.playlist.data
             my_id = \
-            requests.get('https://api.spotify.com/v1/me', headers={'Authorization': 'Bearer ' + TOKEN_DATA}).json()[
-                'id']
-            playlist_id = requests.post(f"https://api.spotify.com/v1/users/{my_id}/playlists",
+                requests.get('https://api.spotify.com/v1/me', headers={'Authorization': 'Bearer ' + TOKEN_DATA}).json()[
+                    'id']
+            PLAYLIST_ID = requests.post(f"https://api.spotify.com/v1/users/{my_id}/playlists",
                                         headers={'Authorization': 'Bearer ' + TOKEN_DATA,
                                                  'Content-Type': 'application/json'},
                                         json={'name': playlist_name, 'description': 'Spotify wrapped game',
@@ -126,6 +147,49 @@ def callback():
                                      "client_secret": CLIENT_SECRET,
                                      }).json()["access_token"]
     return redirect(url_for("admin"))
+
+
+# @app.route('/play')
+# def play():
+#     global playing
+#     if not playing:
+#         response = requests.get('https://api.spotify.com/v1/me/player/currently-playing',
+#                                 headers={'Authorization': 'Bearer ' + TOKEN_DATA}).json()
+#         print(response['currently_playing_type'])
+#     playing = True
+#     return render_template('play.html', track=response)
+#
+#
+# @app.route('/play/previos')
+# def previos():
+#     response = requests.post("https://api.spotify.com/v1/me/player/previous",
+#                              headers={'Authorization': 'Bearer ' + TOKEN_DATA})
+#     print(response.json())
+#
+#     return redirect(url_for('play'))
+#
+#
+# @app.route('/play/play_song')
+# def play_song():
+#     if playing:
+#         response = requests.put('https://api.spotify.com/v1/me/player/play',
+#                                 headers={'Authorization': 'Bearer ' + TOKEN_DATA, 'Content-Type': 'application/json'})
+#         print(response.json())
+#
+#     else:
+#         response = requests.put('https://api.spotify.com/v1/me/player/pause',
+#                                 headers={'Authorization': 'Bearer ' + TOKEN_DATA})
+#         print(response.json())
+#
+#     return redirect(url_for('play'))
+#
+#
+# @app.route('/play/next')
+# def next():
+#     response = requests.post("https://api.spotify.com/v1/me/player/next",
+#                              headers={'Authorization': 'Bearer ' + TOKEN_DATA})
+#     print(response.json())
+#     return redirect(url_for('play'))
 
 
 if __name__ == "__main__":
